6c971dfc73d4b33e199dd390fdea4ae98f77274d
Ako520 <jingnanbendan@163.com>
2019-02-11 18:27:09 +0800

    go

---
+++ precache-manifest.531be50df1e8b32a1e42485903b1fd6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8069efbf0069dfe168ea",
+    "url": "/static/js/main.8069efbf.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "8069efbf0069dfe168ea",
+    "url": "/static/css/main.6723fe09.chunk.css"
+  },
+  {
+    "revision": "c2415c93805efda8256105b063b57326",
+    "url": "/index.html"
+  }
+];
